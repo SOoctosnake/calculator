733f5714f43119a9ccd116878f89a2679c223809
praja <praja@KulkarniDesktop>
2022-08-20 11:00:53 -0700

    adding calculator

---
+++ src/calculator_github/caluculator.java
@@ -0,0 +1,16 @@
+package calculator_github;
+
+public class caluculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float answer = add(2,43);
+		System.out.println(answer);
+
+	}
+	
+	public static float add(float a, float b) {
+		return(a+b);
+	}
+	
+}
